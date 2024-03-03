916db7ac5b7c166c2cacdc2a8ac3fcea0957fa27
Nurye Nigus <70792057+Nurye-Nigus@users.noreply.github.com>
2024-03-03 08:36:32 +0000

    after rearanging

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
- 
 
-#lets creat a route
-@app.route("/")
-def hello_nurye():
-  return render_template("home.html")
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
