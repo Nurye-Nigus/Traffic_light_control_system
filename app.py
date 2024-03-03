58afa157d180378cd5abb846bc9072e6fd8d0558
Nurye Nigus <70792057+Nurye-Nigus@users.noreply.github.com>
2024-03-03 10:50:46 +0000

    upload my notebook file

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/display')
+def display():
+    return render_template('display.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/workflow')
+def workflow():
+    return render_template('workflow.html')
 if __name__ == '__main__':
     app.run(debug=True)
