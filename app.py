cc390fa4028c8dbe4be0d9360d37d06c34fecc04
Nurye Nigus <70792057+Nurye-Nigus@users.noreply.github.com>
2024-03-10 00:47:17 +0000

    dynamic changes on the squires list

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template
+from replit import db
+
+# Connect to MySQL database
+conn = db.connect('mysql')
 
 app = Flask(__name__)
 
+squires = [
+    {
+        "name": "Stadium",
+        "junctions": "From-Megenagn, From-Agona, From-Mexico, From-Piasa",
+        "map_location": "https://maps.app.goo.gl/6UC9EtcyaH6MjEQZA",
+        "morning_manager": "Mr. X +251929404324",
+        "afternoon_manager": "Mr. Y +251929404325",
+        "evening_manager": "Mr. Z +251929404326"
+    },
+    {
+        "name": "Mexico",
+        "junctions": "Junction A, Junction B, Junction C",
+        "map_location": "https://maps.app.goo.gl/MpXZLGxuiYP3toyH6",
+        "morning_manager": "Mr. A +251929404324",
+        "afternoon_manager": "Mr. B +251929404324",
+        "evening_manager": "Mr. C +251929404324"
+    },
+    {
+        "name": "Megenagna",
+        "junctions": "Junction X, Junction Y, Junction Z",
+        "map_location": "https://maps.app.goo.gl/vKvPeNNkVM8K1CgN7",
+        "morning_manager": "Mr. P +251929404324",
+        "afternoon_manager": "Mr. Q +251929404324",
+        "evening_manager": "Mr. R +251929404324"
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', squires=squires)
 
 @app.route('/display')
 def display():
